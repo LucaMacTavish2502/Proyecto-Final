aea3d6ca90b2048a1221185bc1ce30c7fcb9a042
Luca Ciz <lucaciz2502@gmail.com>
2024-01-09 17:05:59 -0300

    github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CargarComponent } from './estudiantes/cargar/cargar.component';
 import { EditarComponent } from './estudiantes/editar/editar.component';
 import { ListarComponent } from './estudiantes/listar/listar.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 const routes: Routes = [
+  {path: 'listarestudiantes',component:ListarComponent},
+  {path: 'editarestudiantes',component:EditarComponent},
+  {path: 'cargarestudiantes',component:CargarComponent},
+  {path: 'mostrarestudiantes',component:ListarComponent},
+  {path: 'añadirestudiantes',component:CargarComponent}
 ];
 
 @NgModule({
